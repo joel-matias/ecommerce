f383ff0952cf1947afd67dece5a537f8194f1123
joel12-Sant <joelsantiagos000@gmail.com>
2026-02-18 20:59:16 -0600

    ui: design base header app layout

---
+++ resources/views/livewire/navigation.blade.php
@@ -0,0 +1,35 @@
+<div>
+    <header class="bg-purple-600">
+        <x-container class="px-4 py-4">
+            <div class="flex space-x-8 items-center">
+
+                <button class="text-3xl">
+                    <i class="fas fa-bars text-white"></i>
+                </button>
+
+                <h1 class="text-white">
+                    <a href="/" class="inline-flex flex-col items-end">
+                        <span class="text-3xl leading-6 font-semibold">
+                            Ecommerce
+                        </span>
+                        <span class="text-xs">
+                            Tienda online
+                        </span>
+                    </a>
+                </h1>
+                <div class="flex-1">
+                    <x-input class="w-full" placeholder="Buscar por producto, tienda o marca" />
+                </div>
+                <div class="space-x-8">
+                    <button class="text-3xl">
+                        <i class="fas fa-user text-white"></i>
+                    </button>
+
+                    <button class="text-3xl">
+                        <i class="fas fa-shopping-cart text-white"></i>
+                    </button>
+                </div>
+            </div>
+        </x-container>
+    </header>
+</div>
