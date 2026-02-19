4ea03df0021295fb8c2cf32d464129790c812924
joel12-Sant <joelsantiagos000@gmail.com>
2026-02-18 21:09:28 -0600

    ui: make header responsive

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,15 +1,15 @@
 <div>
     <header class="bg-purple-600">
         <x-container class="px-4 py-4">
-            <div class="flex space-x-8 items-center">
+            <div class="flex justify-between space-x-8 items-center">
 
-                <button class="text-3xl">
+                <button class="text-xl md:text-2xl lg:text-3xl">
                     <i class="fas fa-bars text-white"></i>
                 </button>
 
                 <h1 class="text-white">
                     <a href="/" class="inline-flex flex-col items-end">
-                        <span class="text-3xl leading-6 font-semibold">
+                        <span class="text-xl md:text-2xl lg:text-3xl leading-4 md:leading-6 font-semibold">
                             Ecommerce
                         </span>
                         <span class="text-xs">
@@ -17,18 +17,22 @@
                         </span>
                     </a>
                 </h1>
-                <div class="flex-1">
+                <div class="flex-1 hidden md:block">
                     <x-input class="w-full" placeholder="Buscar por producto, tienda o marca" />
                 </div>
-                <div class="space-x-8">
-                    <button class="text-3xl">
+                <div class="flex items-centerspace-x-8">
+                    <button class="text-xl md:text-2xl lg:text-3xl">
                         <i class="fas fa-user text-white"></i>
                     </button>
 
-                    <button class="text-3xl">
+                    <button class="text-xl md:text-2xl lg:text-3xl">
                         <i class="fas fa-shopping-cart text-white"></i>
                     </button>
                 </div>
+            </div>
+
+            <div class="mt-4 md:hidden">
+                <x-input class="w-full" placeholder="Buscar por producto, tienda o marca" />
             </div>
         </x-container>
     </header>
