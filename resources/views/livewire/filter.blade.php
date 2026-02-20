6eacb1bded854489d4a816afcc99e9136fc6dadf
joel12-Sant <joelsantiagos000@gmail.com>
2026-02-20 15:05:21 -0600

    ui: make family page responsive

--- resources/views/livewire/filter.blade.php
+++ resources/views/livewire/filter.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-white py-12 ">
-    <x-container class="flex px-4">
+    <x-container class="md:flex px-4">
         @if (count($options))
-            <aside class="w-52 flex-shrink-0 mr-8">
+            <aside class="md:w-52 md:flex-shrink-0 md:mr-8 mb-8 md:mb-0">
                 <ul class="space-y-4">
                     @foreach ($options as $option)
                         <li x-data="{
@@ -35,7 +35,7 @@
             </aside>
         @endif
 
-        <div class="flex-1">
+        <div class="md:flex-1">
             <div class="flex items-center">
                 <span class="mr-2">
                     Ordenar por:
@@ -54,7 +54,7 @@
             </div>
 
             <hr class="my-4">
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 @foreach ($products as $product)
                     <article class="bg-white shadow rounded overflow-hidden">
                         <img src="{{ $product->image }}" class="w-full h-48 object-cover object-center">
