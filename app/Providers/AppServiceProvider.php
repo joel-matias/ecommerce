adf2ebfb37970e09a6ec020263b7ff8bd609af35
joel12-Sant <joelsantiagos000@gmail.com>
2026-03-02 12:11:35 -0600

    feat: add order PDF generation using observers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Cover;
+use App\Models\Order;
 use App\Observers\CoverOserver;
+use App\Observers\OrderObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Cover::observe(CoverOserver::class);
+        Order::observe(OrderObserver::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ShippingController;
 use App\Http\Controllers\SubCategoryController;
 use App\Http\Controllers\WelcomeController;
-use Gloudemans\Shoppingcart\Facades\Cart;
+use App\Models\Order;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome.index');
@@ -39,8 +40,14 @@ Route::middleware([
     })->name('dashboard');
 });
 
-Route::get('pruebaa', function () {
-    Cart::instance('shopping');
+Route::get('prueba', function () {
+    $order = Order::first();
 
-    return Cart::content();
+    $pdf = Pdf::loadView('orders.ticket', compact('order'))->setPaper('a5');
+
+    $pdf->save(storage_path('app/public/tickets/ticket-'.$order->id.'.pdf'));
+
+    $order->pdf_path = 'tickets/ticket-'.$order->id.'.pdf';
+
+    $order->save();
 });
