3b76bf3a56e45cc3278cf57594c3343ee33aef68
joel-matias <joelsantiagos000@gmail.com>
2026-03-07 15:43:32 -0600

    feat: add driver listing

--- resources/views/admin/drivers/index.blade.php
+++ resources/views/admin/drivers/index.blade.php
@@ -6,11 +6,12 @@
     [
         'name' => 'Conductores',
     ],
-
 ]">
     <x-slot name="action">
         <a href="{{ route('admin.drivers.create') }}" class="btn btn-blue">
             nuevo
         </a>
     </x-slot>
+
+    @livewire('admin.drivers.driver-table')
 </x-admin-layout>
