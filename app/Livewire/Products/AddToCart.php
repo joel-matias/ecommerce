408748f5dd385d9d2c6643de17c0a5e10438c32b
joel-matias <joelsantiagos000@gmail.com>
2026-03-08 13:23:24 -0600

    refactor: merge add-to-cart-variant into add-to-cart

--- app/Livewire/Products/AddToCart.php
+++ app/Livewire/Products/AddToCart.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire\Products;
 
+use App\Models\Feature;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class AddToCart extends Component
@@ -11,6 +13,23 @@ class AddToCart extends Component
     public $product;
 
     public $qty = 1;
+
+    public $selectedFeatures = [
+
+    ];
+
+    public function mount()
+    {
+        $this->selectedFeatures = $this->product->variants->first()->features->pluck('id','option_id')->toArray();
+    }
+
+    #[Computed]
+    public function variant()
+    {
+        return $this->product->variants->filter(function ($variant) {
+            return ! array_diff($variant->features->pluck('id')->toArray(), $this->selectedFeatures);
+        })->first();
+    }
 
     public function add_to_cart()
     {
@@ -23,8 +42,9 @@ class AddToCart extends Component
             'price' => $this->product->price,
             'options' => [
                 'image' => $this->product->image,
-                'sku' => $this->product->sku,
-                'features' => [],
+                'sku' => $this->variant->sku,
+                'features' => Feature::whereIn('id', $this->selectedFeatures)
+                    ->pluck('description', 'id')->toArray(),
             ],
         ]);
 
